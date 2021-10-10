9d97f487c7404c59f210864cc7cd53822dcd689c
Graeme Winter <graeme.winter@gmail.com>
2021-10-10 06:37:25 +0100

    Initial sim

---
+++ card_simulator.py
@@ -0,0 +1,42 @@
+import argparse
+import random
+import sys
+
+import numpy as np
+
+
+def simulate(N, M, P, R):
+    """Simulate collection process of N unique items, with M gathered in
+    in advance, with P unique per pack, leaving R, return number of packs"""
+    collect = set(range(N - M))
+    all = list(range(N))
+
+    n = 0
+    while len(collect) > R:
+        pack = random.sample(all, P)
+        collect -= set(pack)
+        n += 1
+    return n
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="Collecting card simulator")
+    parser.add_argument("--count", type=int, default=144)
+    parser.add_argument("--collected", type=int, default=0)
+    parser.add_argument("--iter", type=int, default=1000)
+    parser.add_argument("--pack", type=int, default=4)
+    parser.add_argument("--remain", type=int, default=0)
+
+    args = parser.parse_args()
+
+    population = np.array(
+        [
+            simulate(args.count, args.collected, args.pack, args.remain)
+            for j in range(args.iter)
+        ]
+    )
+    q1, med, q3 = tuple(map(int, np.percentile(population, [25, 50, 75])))
+    minmax = population.min(), population.max()
+    print(f"Min / max: {minmax[0]} / {minmax[1]}")
+    print(f"Quartiles: {q1} / {med} / {q3}")
